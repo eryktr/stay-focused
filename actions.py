4f897befd49c7c1f2ba18dae137daa91f6957e0b
eryktrzeciakiewicz <eryk.trzeciakiewicz@gmail.com>
2019-02-09 14:40:55 +0100

    Implemented help message

--- actions.py
+++ actions.py
@@ -1,3 +1,12 @@
 def print_welcome_message():
     print("Welcome to stay-focused!")
     print("Hope you have some great, productive time!")
+
+
+def print_help():
+    print("work <minutes> - initializes a working session which takes exactly <minutes> minutes.")
+    print("After a working session is finished, you will be notified.")
+    print("break <minutes> - initializes a break which takes exaclty <minutes>")
+    print("Once the break is over, you will be regularly notified that you should go back to your work " \
+        "until you start a new working session.")
+    print("type 'exit' to end the program.")
--- interface.py
+++ interface.py
@@ -2,3 +2,4 @@ import actions
 
 if __name__ == "__main__":
     actions.print_welcome_message()
+    actions.print_help()
